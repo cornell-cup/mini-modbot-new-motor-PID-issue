bc12d7662fd148adab87cb5d413dc5e2b6c702ba
stahmid <sm893@cornell.edu>
2016-04-20 22:47:39 -0400

    Added separate file for pid controller
    Move pid controller as separate function and pass pid structure, set
    point (reference speed)

---
+++ MotorEncoder/src/pid_controller.c
@@ -0,0 +1,6 @@
+
+#include "pid_controller.h"
+
+int motor_pid(pid_t* pid, int target_speed, int motor_speed){
+
+}
---
+++ MotorEncoder/src/pid_controller.h
@@ -0,0 +1,21 @@
+/*
+ * pid_controller.h
+ *
+ *  Created on: Apr 20, 2016
+ *      Author: CornellCup
+ */
+
+#ifndef PID_CONTROLLER_H_
+#define PID_CONTROLLER_H_
+
+typedef struct{
+	float Kp;
+	float Ki;
+	float Kd;
+	int accumulator;
+}pid_t;
+
+#define PID_MAX		1000
+#define PID_MIN		(-PID_MAX)
+
+#endif /* PID_CONTROLLER_H_ */
